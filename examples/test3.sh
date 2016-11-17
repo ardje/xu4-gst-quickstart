aeee55e91a8e1afe8012148b1b2cee92ed21afa7
Odroid @ odroid9 <ard@kwaak.net>
2016-11-17 15:21:39 +0000

    Fixe test description

--- examples/test3.sh
+++ examples/test3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 . ./whatdevices.sh
-# Same as test2, using 1080p file
+# Stream the raw mp4 file without decode and encode
 time gst-launch-1.0 filesrc location=~/Videos/sintel_trailer-1080p.mp4 ! qtdemux ! h264parse config-interval=2 ! mpegtsmux ! rtpmp2tpay ! queue ! udpsink host=239.255.12.42 port=5004 auto-multicast=true sync=true
