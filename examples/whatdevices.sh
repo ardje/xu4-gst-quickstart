e67217f67ca9fc7861eea04bc4973010bc607e5d
Odroid add test system <ard@kwaak.net>
2017-08-30 15:11:33 +0000

    Add converters

--- examples/whatdevices.sh
+++ examples/whatdevices.sh
@@ -1,5 +1,7 @@
 DECODER=$(gst-inspect-1.0 | awk '/v4l2video[0-9]videodec/ { gsub(":",""); print $2 ; nextfile }')
 ENCODER=$(gst-inspect-1.0 | awk '/v4l2video[0-9]+h264enc/ { gsub(":",""); print $2 ; nextfile }')
+CONVERT1=$(gst-inspect-1.0 | awk '/v4l2video[0-9]+convert/ { gsub(":",""); print $2 ; nextfile }'|head -1)
+CONVERT2=$(gst-inspect-1.0 | awk '/v4l2video[0-9]+convert/ { gsub(":",""); print $2  }'|tail -1)
 
 # If you have webcam, define it here:
 DEVICE=$(echo /dev/v4l/by-id/usb-Epiphan*)
