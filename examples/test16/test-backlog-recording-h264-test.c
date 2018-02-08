411f2b7df3386618be7696ee781cc370ba6ff66b
Sam Van Den Berge <sam.van.den.berge@telenet.be>
2018-02-08 16:51:20 +0100

    s/v4l2video30convert/v4l2video20convert/
    Signed-off-by: Sam Van Den Berge <sam.van.den.berge@telenet.be>

--- examples/test16/test-backlog-recording-h264-test.c
+++ examples/test16/test-backlog-recording-h264-test.c
@@ -517,7 +517,7 @@ main (int argc, char **argv)
     app.file_modulo = 500;
     app.pipeline = gst_parse_launch ("v4l2src name=v4l2src device=/dev/video0 do-timestamp=true "
       " ! video/x-raw, format=YUY2,width=1280,height=720,framerate=30/1 "
-      " ! v4l2video30convert ! video/x-raw, format=NV12 "
+      " ! v4l2video20convert ! video/x-raw, format=NV12 "
       " ! v4l2video11h264enc extra-controls=encode,h264_level=10,h264_profile=4,frame_level_rate_control_enable=1,video_bitrate=4194304 "
       " ! h264parse config-interval=2 "
       " ! queue name=vrecq "
